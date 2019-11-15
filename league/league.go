18a1877ce475428dccbe84e8585652e395e31898
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-11-16 00:40:12 +0100

    more minimax players in league

--- league/league.go
+++ league/league.go
@@ -21,7 +21,11 @@ func main() {
 	t.AddPlayer("Conny Corner", player.NewCornerPlayer)
 	t.AddPlayer("Edgy Edge", player.NewEdgePlayer)
 	t.AddPlayer("Max Round", player.NewMaxroundPlayer)
-	t.AddPlayer("Mini Max", player.NewMinimaxPlayerSpawnFunc(4))
+	t.AddPlayer("Mini Max I.", player.NewMinimaxPlayerSpawnFunc(1))
+	t.AddPlayer("Mini Max II.", player.NewMinimaxPlayerSpawnFunc(2))
+	t.AddPlayer("Mini Max III.", player.NewMinimaxPlayerSpawnFunc(3))
+	t.AddPlayer("Mini Max IV.", player.NewMinimaxPlayerSpawnFunc(4))
+	t.AddPlayer("Mini Max V.", player.NewMinimaxPlayerSpawnFunc(5))
 	r, err := t.Play(*numberOfRounds)
 	if err != nil {
 		log.Print(err)
