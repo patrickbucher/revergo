da187278bc3028e8bc4497033638732ec8fd8a3a
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-12-30 13:23:44 +0100

    only SVG trees, SVG tree with depth 7

--- game-trees/create-game-trees.sh
+++ game-trees/create-game-trees.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-for depth in {2..6}
+for depth in {2..7}
 do
     go run ../graph/gametree.go -depth $depth > tmp.dot
     dot -Tsvg tmp.dot -o tree-${depth}.svg
-    dot -Tpng tmp.dot -o tree-${depth}.png
     rm -f tmp.dot
 done
